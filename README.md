f8b2ab6fb516a65333498500eef1d0a79bca231e
jp-gerona <julianpetergerona@gmail.com>
2025-05-23 15:41:02 +0800

    feat: startpage first config

---
+++ README.md
@@ -0,0 +1,3 @@
+# My YAGS startpage
+
+This is my custom Firefox CSS Start/New Page created using [YAGS](https://github.com/PrettyCoffee/yet-another-generic-startpage?tab=readme-ov-file) (Yet Another Generic Startpage) by PrettyCoffee.
---
+++ scripts.js
@@ -0,0 +1,95 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+
+/**
+ * Search function
+ */
+
+const searchInput = document.querySelector("#searchbar > input")
+const searchButton = document.querySelector("#searchbar > button")
+
+const lookup = {"/":"/","reddit":"https://reddit.com/","maps":"https://maps.google.com/","chatgpt":"https://chatgpt.com/","gemini":"https://gemini.google.com/app","facebook":"https://www.facebook.com","messenger":"https://messenger.com/","#new":"","instagram":"https://instagram.com"}
+const engine = "google"
+const engineUrls = {
+  deepl: "https://www.deepl.com/translator#-/-/{query}",
+  duckduckgo: "https://duckduckgo.com/?q={query}",
+  ecosia: "https://www.ecosia.org/search?q={query}",
+  google: "https://www.google.com/search?q={query}",
+  startpage: "https://www.startpage.com/search?q={query}",
+  youtube: "https://www.youtube.com/results?q={query}",
+}
+
+const isWebUrl = value => {
+  try {
+    const url = new URL(value)
+    return url.protocol === "http:" || url.protocol === "https:"
+  } catch {
+    return false
+  }
+}
+
+const getTargetUrl = value => {
+  if (isWebUrl(value)) return value
+  if (lookup[value]) return lookup[value]
+  const url = engineUrls[engine] ?? engine
+  return url.replace("{query}", value)
+}
+
+const search = () => {
+  const value = searchInput.value
+  const targetUrl = getTargetUrl(value)
+  window.open(targetUrl, "_self")
+}
+
+searchInput.onkeyup = event => event.key === "Enter" && search()
+searchButton.onclick = search
+
+/**
+ * inject bookmarks into html
+ */
+
+const bookmarks = [{"id":"7aK6JDt5AAWZ5GKJ","label":"school","bookmarks":[{"id":"0jrFbzs2RPspvzIR","label":"blackboard","url":"https://mcl.blackboard.com/ultra"},{"id":"cIM8uKNViS2FaLEL","label":"teams","url":"https://teams.microsoft.com/v2/"},{"id":"tgonLboZDrjJKT5b","label":"coursera","url":"https://www.coursera.org/programs/mapua-university-batch-2016-xbv1a?authProvider=mapua"}]},{"id":"7minpSzW6v6N67Ty","label":"code","bookmarks":[{"id":"2xXPDNhMkNLjHzqR","label":"github","url":"https://github.com/"},{"id":"6fupJOo5XyxbSiwa","label":"figma","url":"https://www.figma.com/"},{"id":"sz5DLqHE4JhYTCX6","label":"postman","url":"https://www.postman.com/"}]},{"id":"wojjqj9xsp0fmsMp","label":"media","bookmarks":[{"id":"Ysu3mLnX21KMUx3Z","label":"youtube","url":"https://youtube.com"},{"id":"jaSyAdIZ67XOJn2G","label":"twitch","url":"https://twitch.com"}]},{"id":"2JltTeXiIDlyrPMH","label":"misc","bookmarks":[{"id":"kE7nnIcEVXlKiMP3","label":"awwwards","url":"https://awwwards.com"}]}]
+
+const createGroupContainer = () => {
+  const container = document.createElement("div")
+  container.className = "bookmark-group"
+  return container
+}
+
+const createGroupTitle = title => {
+  const h2 = document.createElement("h2")
+  h2.innerHTML = title
+  return h2
+}
+
+const createBookmark = ({ label, url }) => {
+  const li = document.createElement("li")
+  const a = document.createElement("a")
+  a.href = url
+  a.innerHTML = label
+  li.append(a)
+  return li
+}
+
+const createBookmarkList = bookmarks => {
+  const ul = document.createElement("ul")
+  bookmarks.map(createBookmark).forEach(li => ul.append(li))
+  return ul
+}
+
+const createGroup = ({ label, bookmarks }) => {
+  const container = createGroupContainer()
+  const title = createGroupTitle(label)
+  const bookmarkList = createBookmarkList(bookmarks)
+  container.append(title)
+  container.append(bookmarkList)
+  return container
+}
+
+const injectBookmarks = () => {
+  const bookmarksContainer = document.getElementById("bookmarks")
+  bookmarksContainer.append()
+  bookmarks.map(createGroup).forEach(group => bookmarksContainer.append(group))
+}
+
+injectBookmarks()
